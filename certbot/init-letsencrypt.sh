36883fe40d4bcf104073e4be28afb8df7d9d7b63
ffuented <85798215+ffuented@users.noreply.github.com>
2024-06-29 09:06:39 +0200

    Update init-letsencrypt.sh

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=(example.com)
+domains=(iomed.health)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="your-email@example.com" # Adding a valid address is strongly recommended
+email="email@iomed.health" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
